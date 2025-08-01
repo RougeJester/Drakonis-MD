444d418314e18555c26ca38a3c802f004300d1c3
RougeJester <letlotlomosatupa01@gmail.com>
2025-08-01 21:08:12 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "1(236)362-1958", // PUT OWNER NUMBER HERE
-  PREFIX: "." // prefix (e.g., ., /, !, *)
+  SESSION_ID: "drakonis~x61kUZ7b#WqUC535mXozVO0vha3dPwikPgEIre9a5YW2FK1fisKs",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "27717796385", // PUT OWNER NUMBER HERE
+  PREFIX: "," // prefix (e.g., ., /, !, *)
 };
